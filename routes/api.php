a4de3bb3b345a3cab94f32f70a112644b5785b48
Billy Chubb <billy@bchubb.co.uk>
2024-03-13 13:00:00 +0000

    working on how commands will interact

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/', function (Request $request) {
+    return json_encode(['foo' => 'bar']);
+});
+
+Route::get('/ping', function (Request $request) {
+    http_response_code(200);
+});
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Http;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Artisan::command('ping', function () {
+    $response = Http::get(env('GRAPPLE_MANAGER_URL') . '/ping');
+    $this->comment($response->ok() ? 'ok' : 'not ok');
+})->purpose('Check status of the manager')->hourly();
+
+Artisan::command('sites', function () {
+    $response = Http::get(env('GRAPPLE_MANAGER_URL') . '/ping');
+})->purpose('Check status of the manager')->hourly();
